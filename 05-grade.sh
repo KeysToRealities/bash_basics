38e7823d963b9fefa0ad3b7e9e54deab61a2757a
Keyvaun <keyvaunroblox@gmail.com>
2025-10-24 21:28:27 -0400

    Added temperature command

--- 05-grade.sh
+++ 05-grade.sh
@@ -19,3 +19,16 @@ fi
 # that prints "it's cold" if the temperature is < 40
 # it's chilly if < 60, it's okay if < 70 and, it's hot for 
 # everything else
+
+echo "what is the temperature outside"	
+echo "give a numeric answer"
+read temp
+if [ "$temp" -lt 40 ]; then
+  echo "It's cold"
+elif [ "$temp" -lt 60 ]; then
+  echo "It's chilly"
+elif [ "$temp" -lt 70 ]; then
+  echo "It's okay"
+else
+  echo "It's hot"
+fi
